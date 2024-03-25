445641e08c0bdc48a9604e42f3c562473e5239b3
miraboh <myrabohuche@gmail.com>
2024-03-25 10:47:26 +0100

    Initial Commit

---
+++ Whether App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Whether App
+//
+//  Created by mac on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Whether App/Whether_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whether_AppApp.swift
+//  Whether App
+//
+//  Created by mac on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Whether_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
